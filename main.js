3b897c11006c417d6a5971528df78abb45fad06a
TJ441 <timothy.johnson@zohomail.com>
2022-11-14 11:24:15 -0600

    add

---
+++ main.js
@@ -0,0 +1,30 @@
+const container = document.querySelector('#container');
+const gridBtn = document.querySelector('#setsize');
+
+gridBtn.addEventListener('click', () => {
+          container.textContent = '';
+          size = prompt("Enter grid size")
+          if (size > 100) return alert("Limit 100")
+          buildGrid(size)
+          })
+
+function buildGrid(size) {
+  let grid = "";
+  
+  for (let i = 0; i < size * size; i++) {
+      let square = (400 / size);
+      grid = document.createElement('div');
+      grid.classList.add('grid');
+      grid.style.width = `${square}px`;
+      grid.style.height = `${square}px`;
+      grid.addEventListener('mouseover', changeColor)
+      container.appendChild(grid);  
+  }   
+}
+
+function changeColor(e) {
+  e.target.style.backgroundColor = 'blue';
+}
+
+
+buildGrid(16);
