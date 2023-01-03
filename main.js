98dbdd3b5ec9980fab7d608749103b42e0665ad9
Tim Johnson <timothy.johnson@zohomail.com>
2023-01-03 06:54:29 -0600

    .

--- main.js
+++ main.js
@@ -3,22 +3,22 @@ const gridBtn = document.querySelector('#setsize');
 
 gridBtn.addEventListener('click', () => {
           container.textContent = '';
-          size = prompt("Enter grid size")
-          if (size > 100) return alert("Limit 100")
+          size = prompt("Enter grid size");
+          if (size > 100)
+            return alert("Limit 100");
           buildGrid(size)
-          })
+          });
 
 function buildGrid(size) {
   let grid = "";
-  
   for (let i = 0; i < size * size; i++) {
       let square = (400 / size);
       grid = document.createElement('div');
       grid.classList.add('grid');
       grid.style.width = `${square}px`;
       grid.style.height = `${square}px`;
-      grid.addEventListener('mouseover', changeColor)
-      container.appendChild(grid);  
+      grid.addEventListener('mouseover', changeColor);
+      container.appendChild(grid);
   }   
 }
 buildGrid(16);
